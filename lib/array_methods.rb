c7a8ab2316f616db8427554af9011544d6813d7d
Guligena Aierken <guligena_aierken@163.com>
2019-10-19 03:35:23 +0000

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,11 +1,15 @@
 def find_element_index(array, value_to_find)
-  # Add your solution here
+    array.find_index(value_to_find)
 end
 
-def find_max_value(array)
+
+
+def find_max_value(array) #array of integers.
+    array.max
   # Add your solution here
 end
 
 def find_min_value(array)
+    array.min
   # Add your solution here
 end
