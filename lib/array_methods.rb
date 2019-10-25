dc9e7f7670800bdbac6a6cc097060de78458a8df
Eric Cave <caveman4949@gmail.com>
2019-10-25 13:42:00 +0000

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,11 +1,18 @@
-def find_element_index(array, value_to_find)
-  # Add your solution here
-end
+require 'spec_helper'
+require 'pry'
 
-def find_max_value(array)
-  # Add your solution here
-end
+describe 'Finding data' do
 
-def find_min_value(array)
-  # Add your solution here
-end
+  scale = [2,3,4,5]
+  scale_2 = [6,5,4,3]
+  hill = [1,2,3,4,5,4,3,2,1]
+  valley = [5,4,3,2,1,0,1,2,3,4,5]
+  zig_zag = [500,4,1000,5,250]
+
+  describe 'find_element_index(array, value_to_find)' do
+    it 'takes in an array and a value and returns the index of that value' do
+      expect(find_element_index(scale,2)).to eq(0)
+      expect(find_element_index(scale,4)).to eq(2)
+      expect(find_element_index(scale_2,5)).to eq(1)
+      expect(find_element_index(scale_2,6)).to eq(0)
+    end
